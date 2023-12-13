31af7f1273cf2d529a5951da887cc7d17c469f0d
utegenovkamel <kamel_utegeno@mail.ru>
2023-12-13 19:03:58 +0600

    add app folder & delete unused files

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,14 +2,13 @@ import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
 import {RouterProvider} from "atomic-router-react";
 import { MantineProvider } from '@mantine/core';
-import { theme } from './theme';
 import {Pages} from "~/pages";
 import {router} from "~/shared/routing.ts";
 import {Layout} from "~/modules";
 
 export default function App() {
   return (
-    <MantineProvider theme={theme}>
+    <MantineProvider>
       <RouterProvider router={router}>
         <Layout>
           <Pages />
--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.tsx';
 import {appStarted} from "~/shared/config";
 
 const container = document.getElementById('root') as HTMLElement;
